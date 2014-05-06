7e68d0ee11217b452eb4bb007f90693c855b1baa
Tam Tran <tambo2000@gmail.com>
2014-05-05 23:54:58 -0700

    more mods, more testing

--- javascripts/views/bookingResults/BookingResultsView.js
+++ javascripts/views/bookingResults/BookingResultsView.js
@@ -11,26 +11,24 @@ define([
 
     render: function(){
       var that = this;
-      this.get_json();
-      var roomTypes = new RoomTypesCollection();
-      roomTypes.fetch({
-        // dataType: 'jsonp',
-        // jsonp: 'jsonp',
-        success: function(roomTypes) {
-          console.log("in success");
-          var renderedTemplate = _.template(bookingResultsTemplate, {roomTypes: roomTypes.models})
-          that.$el.html(bookingResultsTemplate);
-        },
-        error: function (collection, resp){
-          console.log("error retrieving model");
-        }
-      })
+      var roomTypes = new RoomTypesCollection(this.get_json());
+      var renderedTemplate = _.template(bookingResultsTemplate, {roomTypes: roomTypes.models})
+      this.$el.html(bookingResultsTemplate);
+      // roomTypes.fetch({
+      //   dataType: 'jsonp',
+      //   success: function(roomTypes) {
+      //     console.log("in success");
+      //     var renderedTemplate = _.template(bookingResultsTemplate, {roomTypes: roomTypes.models})
+      //     that.$el.html(bookingResultsTemplate);
+      //   },
+      //   error: function (collection, resp){
+      //     console.log("error retrieving model");
+      //   }
+      // })
     },
 
     get_json: function() {
       console.log("getting url contents");
-      var something = $.get("php/get_url_contents.php");
-      console.log(something.responseText);
       var msg = $.ajax({type: "GET", url: "php/get_url_contents.php", async: false}).responseText;
       msg = msg.replace("<br/>", "");
       console.log(msg);
